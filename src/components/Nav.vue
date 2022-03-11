f47b4eb31996ab0e39144aedaf22510aaddfc701
drdepe <drdepe7@gmail.com>
2022-03-11 13:30:41 +0200

    2

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,16 @@
 <div>
  <nav>
     <v-toolbar  flat class="deep-purple accent-10" >
+         <v-icon class="white--text vert_hover" size="35px" v-on:click='() =>{drawer=!drawer}'>menu</v-icon>
         <v-spacer></v-spacer>
-    <v-icon class="white--text vert_hover" size="35px" v-on:click='() =>{drawer=!drawer}'>menu</v-icon>
+
+        <v-toolbar-title class="white--text">
+            <div class="fmsi mt-2">
+                            <p class="uni_name mr-2 font">كلية العلوم الرياضية والمعلوماتية</p>
+                <img src="../assets/uofk.png" alt="" class="">
+            </div>
+            
+        </v-toolbar-title>
     </v-toolbar>
 
     </nav>
@@ -36,16 +44,24 @@ data(){
 </script>
 
 <style scoped>
-/* .vert_hover{
-    transition: 1s all;
-
-}
-.vert_hover:hover{
+@import url('https://fonts.googleapis.com/css2?family=Lalezar&display=swap');
+/* .vert_hover:hover{
 background-color:#000000;
 border-radius: 11px;
-} */
+}  */
 .poin{
     cursor:pointer;
     text-decoration: none;
+}
+.fmsi img{
+    width: 42px;
+    height: 42px;
+    border-radius: 90px;
+    display: inline-block;
+}
+.uni_name{
+    font-family: 'Lalezar', cursive;
+    display: inline-block;
+    font-size:20px;
 }
 </style>
